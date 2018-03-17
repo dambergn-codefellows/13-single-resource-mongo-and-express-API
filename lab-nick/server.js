9e782ac9ea8eb51bb7cab214926cd0bb75ba6ac9
dambergn <dambergn@gmail.com>
2018-03-17 09:46:05 -0700

    set up scafolding.

---
+++ lab-nick/server.js
@@ -0,0 +1,9 @@
+'use strict';
+const express = require('express');
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port: ${PORT}, use CTRL+C to stop.`);
+});
